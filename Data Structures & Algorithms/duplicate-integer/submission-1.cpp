92b3544102ba94bf6fbe3d5ef65aa003b4673dc9
Shubham Sahu <153462475+shubhamsahu1611@users.noreply.github.com>
2026-07-22 22:44:44 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        for(auto it:nums) cnt[it]++;
+        for(auto it:cnt) if(it.second>1) return true;
+        return false;
+    }
+};
