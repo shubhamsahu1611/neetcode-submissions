e2711d86cdfe512c8a0d689e327be2254e37cffe
Shubham Sahu <153462475+shubhamsahu1611@users.noreply.github.com>
2026-08-05 21:52:57 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+   public:
+    bool canJump(vector<int>& nums) {
+        int curr_max = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > curr_max) return false;
+            curr_max = max(curr_max, i + nums[i]);
+        }
+        return true;
+    }
+};
